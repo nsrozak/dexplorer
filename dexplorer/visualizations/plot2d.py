554413d72f814302efd25b612dcde7f821350905
nsrozak <natalie.s.rozak@gmail.com>
2023-08-28 18:06:50 -0400

    Change to parent/child class relationships

--- dexplorer/visualizations/plot2d.py
+++ dexplorer/visualizations/plot2d.py
@@ -34,23 +34,6 @@ class Plot2D():
                                    'gold', 'orangered', 'royalblue',
                                    'navajowhite', 'yellowgreen'
                                    ]
-
-    def _raise_value_errors(plot_type: str='line', color_type: str='categorical'):
-        if (color_type == 'density') and (plot_type in ['line']):
-            raise ValueError('color_type `density` cannot be used with plot_type ', plot_type)
-
-    def _get_plot_func(self, plot_type: str='line'): 
-        # get information about the plot
-        if plot_type == 'line':
-            plot_func = plt.plot
-            add_error = True
-
-        elif plot_type == 'scatter':
-            plot_func = plt.scatter
-            add_error = False
-
-        # return values
-        return plot_func, add_error
     
     def _get_error(self, data: pd.DataFrame):
         # get errors if different widths for high and low
@@ -65,21 +48,13 @@ class Plot2D():
 
         # return errors
         return error_low, error_high
-
-    def _add_plot_data(self, fig: Any, data: pd.DataFrame, plot_func: Any, color, add_error: bool):
-        # create plot
-        x = data[self.x_column]
-        y = data[self.y_column]
-        plot_func(x, y, color=color)
-
-        # add error
-        if add_error == True:
-            if self.error_column != None:
-                error_low, error_high = self._get_error(data)
-                plt.fill_between(x, error_low, error_high, color=color, alpha=0.3)
-
-        # return fig
-        return fig
+    
+    def _get_color_density(self, x: pd.Series, y: pd.Series):
+        xy = np.vstack([x,y])
+        z = gaussian_kde(xy)(xy)
+        idx = z.argsort()
+        x, y, z = x[idx], y[idx], z[idx]
+        return x, y, z
         
     def _get_plot_axes(self, title: str, x_ticks_width: int=None, y_ticks_width: int=None):
         # add axes
@@ -96,46 +71,124 @@ class Plot2D():
 
         # return figure
         return fig
-    
-    def _add_categorical_plot_data(self, fig: Any, color_column: str, plot_func: Any, add_error: bool):
+
+    def _add_categorical_plot_data(self, fig: Any, color_column: str, _add_plot_data: function):
         # add legend
         categories = self.data[color_column].unique()
-        plt.legend(['Train', 'Test'], bbox_to_anchor=(1.01, 1), loc='upper left', ncol=1)
+        plt.legend(categories, bbox_to_anchor=(1.01, 1), loc='upper left', ncol=1)
 
         for i, category in enumerate(categories):
             # add data to the figure
             color = self.categorical_colors[i]
             data_category = self.data[self.data[color_column] == category].copy()
-            fig = self._add_plot_data(fig, data_category, plot_func, color, add_error)
+            fig = _add_plot_data(fig, data_category, color=color)
 
         # return figure
         return fig
     
-    def _get_color_density(self, x: pd.Series, y: pd.Series):
-        xy = np.vstack([x,y])
-        z = gaussian_kde(xy)(xy)
-        idx = z.argsort()
-        x, y, z = x[idx], y[idx], z[idx]
-        return x, y, z
 
-    def create_plot(self, title: str, plot_type: str='line', color_type: str='categorical', 
-                    color_column: str=None, x_ticks_width: int=None, y_ticks_width: int=None):
-        # raise value errors
-        self._raise_value_errors(plot_type=plot_type, color_type=color_type)
+class Scatterplot(Plot2D):
+    def __init__(self, data: pd.DataFrame, x_column: str, y_column: str, 
+                 x_label: str, y_label: str, figsize: Tuple=(8, 6)):
+        # initialize super class
+        super(Scatterplot, self).__init__(data, x_column, y_column, 
+                                          x_label, y_label, 
+                                          error_column=None, 
+                                          error_high_column=None, 
+                                          figsize=figsize
+                                          )
+        
+        # set member variables
+        self.diverging_cmap = 'RdYlGn'
+        self.sequential_cmap = 'YlOrRd'
 
-        # get plot type
-        plot_func, add_error = self._get_plot_func(plot_type=plot_type)
+    def _add_plot_data(self, fig: Any, data: pd.DataFrame, color: Any=None, 
+                       diverging: bool=False):
+        # get color
+        color = self.categorical_colors[0] if color == None else color
+        # get color map
+        cmap = self.diverging_cmap if diverging == True else self.sequential_cmap
+
+        # create plot
+        x = data[self.x_column]
+        y = data[self.y_column]
+        plt.scatter(x, y, color=color, cmap=cmap)
+
+        # return figure
+        return fig
+    
+    def _add_density_plot_data(self, fig: Any):
+        # get coloring
+        x = self.data[self.x_column]
+        y = self.data[self.y_column]
+        x, y, z = self._get_color_density(x, y)
+
+        # add plot data
+        fig = self._add_plot_data(fig, self.data, color=z)
+
+        # return figure
+        return fig
+    
+    def create_plot(self, title: str, color_type: str='categorical', color_column: str=None, 
+                    x_ticks_width: int=None, y_ticks_width: int=None):
 
         # get plot axes
         fig = self._get_plot_axes(title, x_ticks_width=x_ticks_width, y_ticks_width=y_ticks_width)
 
         # add data to the plot
-        if (color_type == 'categorical') and (color_column != None):
-           fig = self._add_categorical_data(fig, color_column, plot_func, add_error)
-        else:
-            x = self.data[self.x_column]
-            y = self.data[self.y_column]
+        if (color_type == 'categorical') and (color_column == None):
+            fig = self._add_plot_data(fig, self.data, color=None)
+            
+        elif color_type == 'categorical':
+           fig = self._add_categorical_plot_data(fig, color_column, self._add_plot_data)
 
+        elif color_type == 'density':
+            fig = self._add_density_plot_data(fig)
+
+        elif (color_type == 'diverging') or (color_type == 'continuous'):
+            to_do = True
+
+        # return figure
         return fig
+    
 
-        # color types can be categorical, density, or diverging/sequential with a prespecified column
+class LineChart(Plot2D):
+    def __init__(self, data: pd.DataFrame, x_column: str, y_column: str, 
+                 x_label: str, y_label: str, error_column: str=None, 
+                 error_high_column: str=None, figsize: Tuple=(8, 6)):        
+        # initialize super class
+        super(Scatterplot, self).__init__(data, x_column, y_column, 
+                                          x_label, y_label, 
+                                          error_column=error_column, 
+                                          error_high_column=error_high_column, 
+                                          figsize=figsize
+                                          )
+
+    def _add_plot_data(self, fig: Any, data: pd.DataFrame, color: str=None):
+        # create plot
+        x = data[self.x_column]
+        y = data[self.y_column]
+        plt.plot(x, y, color=color)
+
+        if self.error_column != None:
+            error_low, error_high = self._get_error(data)
+            plt.fill_between(x, error_low, error_high, color=color, alpha=0.3)
+
+        # return figure
+        return fig
+    
+    def create_plot(self, title: str, color_column: str=None, x_ticks_width: int=None, 
+                    y_ticks_width: int=None):
+        # get plot axes
+        fig = self._get_plot_axes(title, x_ticks_width=x_ticks_width, y_ticks_width=y_ticks_width)
+
+        # add data to the plot
+        if color_column == None:
+            fig = self._add_plot_data(fig, self.data, color=None)
+            
+        else:
+           fig = self._add_categorical_plot_data(fig, color_column, self._add_plot_data)
+
+        # return figure
+        return fig
+    
